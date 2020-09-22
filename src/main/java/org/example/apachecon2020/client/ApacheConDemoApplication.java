2a40ce7d268e5ec159f2bb7404e255f10c244710
Patrick Johnson <pjohnson@pivotal.io>
2020-09-22 16:13:19 -0700

    CQ

--- src/main/java/org/example/apachecon2020/client/ApacheConDemoApplication.java
+++ src/main/java/org/example/apachecon2020/client/ApacheConDemoApplication.java
@@ -1,15 +1,24 @@
 package org.example.apachecon2020.client;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
 import org.springframework.data.gemfire.config.annotation.EnableClusterConfiguration;
+import org.springframework.data.gemfire.config.annotation.EnableContinuousQueries;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.listener.annotation.ContinuousQuery;
+
+import org.apache.geode.cache.query.CqEvent;
 
 @SpringBootApplication
 @EnableEntityDefinedRegions
 @EnableClusterConfiguration(useHttp = true, requireHttps = false)
+@ClientCacheApplication(subscriptionEnabled = true)
 public class ApacheConDemoApplication {
 
 	public static void main(String[] args) {
@@ -19,22 +28,24 @@ public class ApacheConDemoApplication {
 	@Bean
 	ApplicationRunner runner(AttendeesRepository repository) {
 		return args -> {
-			Attendee jack = new Attendee(1L, "Jack", "Black");
-			Attendee emily = new Attendee(2L, "Emily", "Black");
-			Attendee janet = new Attendee(3L, "Janet", "Lee");
+			List<Attendee> attendees = new LinkedList<>();
+			attendees.add(new Attendee(1L, "Jack", "Black"));
+			attendees.add(new Attendee(2L, "Emily", "Black"));
+			attendees.add(new Attendee(3L, "Janet", "Lee"));
+			attendees.add(new Attendee(4L, "Robert", "Smith"));
+			attendees.add(new Attendee(5L, "Amanda", "Howard"));
+			attendees.add(new Attendee(6L, "Greg", "Logan"));
 
-			repository.save(jack);
-			repository.save(emily);
-			repository.save(janet);
-
-			System.out.println("Finding all attendees with last name \"Black\": ");
-			repository.findAllByLastName("Black").forEach(System.out::println);
-
-			System.out.println("Finding all attendees whose first name starts with \"J\": ");
-			repository.findAllByFirstNameStartsWith("J").forEach(System.out::println);
-
-			System.out.println("Finding all attendees whose id is NOT \"1\" using a query: ");
-			repository.findAllByIdNotUsingQuery(1L).forEach(System.out::println);
+			for(Attendee attendee : attendees) {
+				System.out.printf("Saving %s%n", attendee);
+				repository.save(attendee);
+				Thread.sleep(2000);
+			}
 		};
+	}
+
+	@ContinuousQuery(name = "AttendeeCQ", query = "SELECT * FROM /Attendees")
+	public void handleEvent(CqEvent event) {
+		System.out.printf("Found %s in Attendees region%n", event.getNewValue());
 	}
 }
